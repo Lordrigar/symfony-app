44e3555fecfe68a3ed228dd2c01101e5571f31a1
lordrigar <wojciechzaorski@gmail.com>
2019-07-19 20:48:30 +0200

    Setup DoctrineODM, Mongo and example endpoints

--- src/Controller/Guest/MongoController.php
+++ src/Controller/Guest/MongoController.php
@@ -4,17 +4,53 @@ namespace App\Controller\Guest;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Document\LogDocument;
+use Doctrine\ODM\MongoDB\DocumentManager;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use DateTimeImmutable;
 
+/**
+ * @Route("/mongo")
+ */
 class MongoController extends AbstractController
 {
     /**
-     * @Route("/mongo", name="mongo")
+     * var DocumentManager
      */
-    public function index()
+    private $dm;
+
+    /**
+     * @param DocumentManager $dm
+     */
+    public function __construct(DocumentManager $dm)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MongoController.php',
-        ]);
+        $this->dm = $dm;
+    }
+
+    /**
+     * @Route("/")
+     */
+    public function index(): JsonResponse
+    {
+        $now = (new DateTimeImmutable())->format('Y-m-d');
+        $message = new LogDocument();
+        $message->setMessage('This is a new mongo message')
+            ->setTimestamp($now);
+
+    
+        $this->dm->persist($message);
+        $this->dm->flush();
+
+        return $this->json('Message saved');
+    }
+
+    /**
+     * @Route("/read")
+     */
+    public function read(): JsonResponse
+    {
+        $logs = $this->dm->getRepository(LogDocument::class)->findAll();
+
+        return $this->json($logs, 200, [], ['groups' => 'LogDocument']);
     }
 }
--- src/Document/LogDocument.php
+++ src/Document/LogDocument.php
@@ -3,6 +3,9 @@
 namespace App\Document;
 
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Serializer\Annotation\Groups;
+use DateTimeImmutable;
+
 /**
  * @MongoDB\Document
  */
@@ -15,11 +18,59 @@ class LogDocument
 
     /**
      * @MongoDB\Field(type="string")
+     * @Groups({"LogDocument"})
      */
     protected $message;
 
     /**
-     * @MongoDB\Field(type="date")
+     * @MongoDB\Field(type="string")
+     * @Groups({"LogDocument"})
      */
-    protected $create_date;
+    protected $timestamp;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return DateTimeImmutable
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @param string $time
+     * @return LogDocument
+     */
+    public function setTimestamp(string $time): LogDocument
+    {
+        $this->timestamp = $time;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     * @return LogDocument
+     */
+    public function setMessage(string $message): LogDocument
+    {
+        $this->message = $message;
+
+        return $this;
+    }
 }
